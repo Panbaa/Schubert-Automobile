13af0386fc1b3c5927dc4ec87937906738f1e7d5
Gilbert <gilbert.lynsche@gmx.de>
2025-01-02 16:59:40 +0100

    Added React Routes and installing Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import Home from './pages/Home'
 import './App.css'
 import Navbar from './components/Navbar'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Reinigung from './pages/Reinigung';
+import SeiteNichtGefunden from './pages/SeiteNichtGefunden';
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Reinigung" element={<Reinigung />} />
+        <Route path="*" element={<SeiteNichtGefunden />} /> 
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
---
+++ src/pages/SeiteNichtGefunden/index.tsx
@@ -0,0 +1,12 @@
+import './SeiteNichtGefunden.css'
+
+const SeiteNichtGefunden = () => {
+  return (
+    <>
+    <h1 className='SeiteNichtGefunden-h1'>Fehler 404</h1>
+    <p className='SeiteNichtGefunden-p'>Die aufgerufene Seite konnte nicht gefunden werden :/</p>
+    </>
+  )
+}
+
+export default SeiteNichtGefunden
