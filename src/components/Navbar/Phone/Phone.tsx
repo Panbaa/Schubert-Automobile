125376d0ed83f0a7f9949b306f873c1f6e9257fc
Gilbert <gilbert.lynsche@gmx.de>
2025-01-28 22:52:12 +0100

    final test for today

--- src/components/Navbar/Phone/Phone.tsx
+++ src/components/Navbar/Phone/Phone.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useState } from "react";
+import { ReactNode, useState } from "react";
 import { INavbarProps } from "../Navbar";
 import { Menu, X } from "lucide-react";
 import "./Phone.css";
