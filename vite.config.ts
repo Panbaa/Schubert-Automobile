e71710a4b87b362003beab94788d87c7e0545831
Gilbert <gilbert.lynsche@gmx.de>
2025-04-29 20:21:33 +0200

    update viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Schubert-Automobile/',
+  base: '/',
   server: {
     proxy: {
       '/api': {
